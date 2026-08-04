d0450c3ed7726b159289bf36bba7610e4c818430
UncleAlexx <itsgdstorm@gmail.com>
2026-08-04 23:16:22 +0300

    C# example

---
+++ C#/FibonacciTermTransmitter.cs
@@ -0,0 +1,5 @@
+﻿
+#:include GlobalUsings.cs
+#:include MathExtensions.cs
+using Extensions;
+Console.WriteLine(Math.GetFibonacciNumber(ToUInt64(args[0])));
---
+++ C#/MathExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Extensions;
+
+public static class MathExtensions
+{
+    extension(Math)
+    {
+        public static ulong GetFibonacciNumber(ulong n, ulong rightTerm = 0, ulong leftTerm = 1, bool flag = true)
+        {
+            ArgumentOutOfRangeException.ThrowIfGreaterThan(n, 94ul, nameof(n));
+            if (n > 1)
+                flag = false;
+            if ((n & 0xFFFFFFFFFFFFFFFE) == 0)
+                return flag ? n : rightTerm;
+            return GetFibonacciNumber(n - 1, leftTerm + rightTerm, 
+                rightTerm == 0 ? leftTerm : rightTerm, flag);
+        }
+    }
+}
